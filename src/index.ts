9baf4b7e460abc85293aaed6ec614c9a42bf04a3
43081j <43081j@users.noreply.github.com>
2019-02-18 23:01:48 +0000

    add to index

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import noClosedShadowRoot from './rules/no-closed-shadow-root';
 import noConstructorAttrs from './rules/no-constructor-attributes';
 import noInvalidElementName from './rules/no-invalid-element-name';
 import noSelfClass from './rules/no-self-class';
+import lifecycleSpelling from './rules/lifecycle-spelling';
 
 export const rules = {
   'attach-shadow-constructor': attachShadowConstructor,
   'guard-super-call': guardSuperCall,
+  'lifecycle-spelling': lifecycleSpelling,
   'no-closed-shadow-root': noClosedShadowRoot,
   'no-constructor-attributes': noConstructorAttrs,
   'no-invalid-element-name': noInvalidElementName,
