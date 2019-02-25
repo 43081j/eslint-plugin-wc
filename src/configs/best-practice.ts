5b9d8484e0311ab7368a1148f89aa5ce11b73e93
43081j <43081j@users.noreply.github.com>
2019-02-25 22:52:00 +0000

    bugfix: inherit recomm config

--- src/configs/best-practice.ts
+++ src/configs/best-practice.ts
@@ -1,13 +1,11 @@
 const config = {
+  extends: ['plugin:wc/recommended'],
   plugins: ['wc'],
 
   rules: {
     'attach-shadow-constructor': 'error',
     'guard-super-call': 'error',
     'no-closed-shadow-root': 'error',
-    'no-constructor-attributes': 'error',
-    'no-invalid-element-name': 'error',
-    'no-self-class': 'error',
     'no-typos': 'error'
   }
 };
