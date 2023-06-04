ed444e57071af2ab74bbbc161880d460332df3e4
43081j <43081j@users.noreply.github.com>
2023-06-04 16:14:42 +0100

    feat (no-constructor): new rule
    This rule allows you to disallow constructors in custom element classes.
    
    It is really a personal preference rule rather than best practice.
    
    Some users may wish to disallow it to avoid problems around reconnection
    to DOM, etc.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import recommended from './configs/recommended';
-import bestPractice from './configs/best-practice';
 import attachShadowConstructor from './rules/attach-shadow-constructor';
+import bestPractice from './configs/best-practice';
 import guardSuperCall from './rules/guard-super-call';
 import noClosedShadowRoot from './rules/no-closed-shadow-root';
+import noConstructor from './rules/no-constructor';
 import noConstructorAttrs from './rules/no-constructor-attributes';
 import noConstructorParams from './rules/no-constructor-params';
 import noInvalidElementName from './rules/no-invalid-element-name';
 import noSelfClass from './rules/no-self-class';
 import noTypos from './rules/no-typos';
+import recommended from './configs/recommended';
 import requireListenerTeardown from './rules/require-listener-teardown';
 
 export const rules = {
   'attach-shadow-constructor': attachShadowConstructor,
   'guard-super-call': guardSuperCall,
   'no-closed-shadow-root': noClosedShadowRoot,
+  'no-constructor': noConstructor,
   'no-constructor-attributes': noConstructorAttrs,
   'no-constructor-params': noConstructorParams,
   'no-invalid-element-name': noInvalidElementName,
