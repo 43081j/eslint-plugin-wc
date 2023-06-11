ed3c951eba69bcae5b1d8156e57aad42c77200d1
43081j <43081j@users.noreply.github.com>
2023-06-11 16:35:37 +0100

    feat (no-customized-built-in-elements): new rule

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import recommended from './configs/recommended';
-import bestPractice from './configs/best-practice';
 import attachShadowConstructor from './rules/attach-shadow-constructor';
+import bestPractice from './configs/best-practice';
 import guardSuperCall from './rules/guard-super-call';
 import noClosedShadowRoot from './rules/no-closed-shadow-root';
 import noConstructorAttrs from './rules/no-constructor-attributes';
 import noConstructorParams from './rules/no-constructor-params';
+import noCustomizedBuiltInElements from './rules/no-customized-built-in-elements';
 import noInvalidElementName from './rules/no-invalid-element-name';
 import noSelfClass from './rules/no-self-class';
 import noTypos from './rules/no-typos';
+import recommended from './configs/recommended';
 import requireListenerTeardown from './rules/require-listener-teardown';
 
 export const rules = {
@@ -16,6 +17,7 @@ export const rules = {
   'no-closed-shadow-root': noClosedShadowRoot,
   'no-constructor-attributes': noConstructorAttrs,
   'no-constructor-params': noConstructorParams,
+  'no-customized-built-in-elements': noCustomizedBuiltInElements,
   'no-invalid-element-name': noInvalidElementName,
   'no-self-class': noSelfClass,
   'no-typos': noTypos,
---
+++ src/rules/no-customized-built-in-elements.ts
@@ -0,0 +1,60 @@
+/**
+ * @fileoverview Disallows extending of built-in elements
+ * @author James Garbutt <https://github.com/43081j>
+ * @author Keith Cirkel <https://github.com/keithamus>
+ */
+
+import {Rule} from 'eslint';
+import * as ESTree from 'estree';
+import {isCustomElement} from '../util';
+import {builtInTagClassList} from '../util/tag-names';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: 'Disallows extending of built-in elements',
+      url: 'https://github.com/43081j/eslint-plugin-wc/blob/master/docs/rules/no-customized-built-in-elements.md'
+    },
+    messages: {
+      noBuiltIn:
+        'You should avoid extending built-in elements as support ' +
+        'is not guaranteed.'
+    }
+  },
+
+  create(context): Rule.RuleListener {
+    // variables should be defined here
+    const source = context.getSourceCode();
+
+    //----------------------------------------------------------------------
+    // Helpers
+    //----------------------------------------------------------------------
+
+    //----------------------------------------------------------------------
+    // Public
+    //----------------------------------------------------------------------
+
+    return {
+      'ClassDeclaration,ClassExpression': (node: ESTree.Class): void => {
+        if (
+          isCustomElement(context, node, source.getJSDocComment(node)) &&
+          node.superClass &&
+          node.superClass.type === 'Identifier' &&
+          node.superClass.name !== 'HTMLElement' &&
+          builtInTagClassList.includes(node.superClass.name)
+        ) {
+          context.report({
+            node,
+            messageId: 'noBuiltIn'
+          });
+        }
+      }
+    };
+  }
+};
+
+export default rule;
