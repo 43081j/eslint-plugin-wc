46912a23af92b1b6170de3d429534b6eb204de17
43081j <43081j@users.noreply.github.com>
2023-06-17 12:34:59 +0100

    feat (max-elements-per-file): new rule

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import recommended from './configs/recommended';
-import bestPractice from './configs/best-practice';
 import attachShadowConstructor from './rules/attach-shadow-constructor';
+import bestPractice from './configs/best-practice';
 import guardSuperCall from './rules/guard-super-call';
+import maxElementsPerFile from './rules/max-elements-per-file';
 import noClosedShadowRoot from './rules/no-closed-shadow-root';
 import noConstructorAttrs from './rules/no-constructor-attributes';
 import noConstructorParams from './rules/no-constructor-params';
 import noInvalidElementName from './rules/no-invalid-element-name';
 import noSelfClass from './rules/no-self-class';
 import noTypos from './rules/no-typos';
+import recommended from './configs/recommended';
 import requireListenerTeardown from './rules/require-listener-teardown';
 
 export const rules = {
   'attach-shadow-constructor': attachShadowConstructor,
   'guard-super-call': guardSuperCall,
+  'max-elements-per-file': maxElementsPerFile,
   'no-closed-shadow-root': noClosedShadowRoot,
   'no-constructor-attributes': noConstructorAttrs,
   'no-constructor-params': noConstructorParams,
