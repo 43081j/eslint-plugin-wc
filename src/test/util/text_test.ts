186c7d263f0df52d931b5ba4f47501adf6ca92ce
43081j <43081j@users.noreply.github.com>
2023-07-24 23:25:03 +0100

    feat (file-name-matches-element): new rule
    Enforces that the filename of a file containing a custom element matches
    its class name according to the configured conventions.
    
    For example, the default is `['kebab', 'camel']` which means the
    following examples are true:
    
    ```ts
    // valid filename: someElement.js
    // invalid filename: some_element.js
    class SomeElement extends HTMLElement {}
    ```
    
    With `matchDirectory: true` set:
    
    ```ts
    // valid filename: foo/bar/someElement.js
    // valid filename: bar/someElement.js
    // valid filename: fooBarSomeElement.js
    // invalid filename: some_element.js
    class FooBarSomeElement extends HTMLElement {}
    ```

---
+++ src/test/util/text_test.ts
@@ -0,0 +1,60 @@
+import * as text from '../../util/text';
+import {expect} from 'chai';
+
+describe('text utils', () => {
+  describe('toKebabCase', () => {
+    it('should convert strings to kebab-case', () => {
+      expect(text.toKebabCase('foobar')).to.equal('foobar');
+      expect(text.toKebabCase('fooBar')).to.equal('foo-bar');
+      expect(text.toKebabCase('FooBar')).to.equal('foo-bar');
+      expect(text.toKebabCase('AfooBfooC')).to.equal('afoo-bfoo-c');
+    });
+  });
+
+  describe('toSnakeCase', () => {
+    it('should convert strings to snake_case', () => {
+      expect(text.toSnakeCase('foobar')).to.equal('foobar');
+      expect(text.toSnakeCase('fooBar')).to.equal('foo_bar');
+      expect(text.toSnakeCase('FooBar')).to.equal('foo_bar');
+      expect(text.toSnakeCase('AfooBfooC')).to.equal('afoo_bfoo_c');
+    });
+  });
+
+  describe('toPascalCase', () => {
+    it('should convert strings to PascalCase', () => {
+      expect(text.toPascalCase('foobar')).to.equal('Foobar');
+      expect(text.toPascalCase('fooBar')).to.equal('FooBar');
+      expect(text.toPascalCase('FooBar')).to.equal('FooBar');
+      expect(text.toPascalCase('AfooBfooC')).to.equal('AfooBfooC');
+      expect(text.toPascalCase('')).to.equal('');
+    });
+  });
+
+  describe('toCamelCase', () => {
+    it('should convert strings to camelCase', () => {
+      expect(text.toCamelCase('foobar')).to.equal('foobar');
+      expect(text.toCamelCase('fooBar')).to.equal('fooBar');
+      expect(text.toCamelCase('FooBar')).to.equal('fooBar');
+      expect(text.toCamelCase('AfooBfooC')).to.equal('afooBfooC');
+      expect(text.toCamelCase('')).to.equal('');
+    });
+  });
+
+  describe('toCaseByType', () => {
+    it('should convert to kebab case with type=kebab', () => {
+      expect(text.toCaseByType('fooBar', 'kebab')).to.equal('foo-bar');
+    });
+
+    it('should convert to camel case with type=camel', () => {
+      expect(text.toCaseByType('FooBar', 'camel')).to.equal('fooBar');
+    });
+
+    it('should convert to snake case with type=snake', () => {
+      expect(text.toCaseByType('fooBar', 'snake')).to.equal('foo_bar');
+    });
+
+    it('should convert to pascal case with type=pascal', () => {
+      expect(text.toCaseByType('fooBar', 'pascal')).to.equal('FooBar');
+    });
+  });
+});
